d48e4e434032706b7478798a8541882653c33cb8
foxikle <foxikle@cytonic.net>
2026-04-27 21:16:04 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Operations:
+- **touch** Create a new file (touch file.txt)
+- **cat** Read a file (cat file.txt)
+- **rm** delete a file (rm file.txt)
+- **>** Write to a file ("Text to write" > file.txt)
+- **chtag** Tag a file in some way (chtag file.txt tags, comma separated, nodel)
+- **help** Prints out a message like this
+
---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import {page} from '$app/state';
+</script>
+
+<div class="h-screen font-title bg-bg flex flex-col justify-center items-center gap-6">
+  <span>
+    <h1 class="text-primary text-shadow-sm text-shadow-muted text-6xl">
+      That's an error: {page.status}
+    </h1>
+    <p class="text-secondary text-3xl">
+      {page.error.message}
+    </p>
+  </span>
+  <span class="text-2xl text-muted italic underline">
+    <a href="/">Go Home</a>
+  </span>
+</div>
+
