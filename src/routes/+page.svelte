c026c2f305f6fe3bfefa795c39d382757f781c34
foxikle <foxikle@cytonic.net>
2026-04-28 06:21:26 -0400

    Code formatting, open source code in new tab

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,34 @@
 <script>
-  
 </script>
 
 <div class="min-h-screen bg-bg">
-  <header class="block w-screen text-center text-6xl text-primary p-6 font-title text-shadow-sm text-shadow-muted">
+  <header
+    class="block w-screen text-center text-6xl text-primary p-6 font-title text-shadow-sm text-shadow-muted"
+  >
     MultiTerm
   </header>
   <div class="flex flex-col justify-center items-center my-6">
-    <div tag="content" class="bg-terminal sm:max-w-[75vw] sm:min-w-[75vw] min-h-[60vh] rounded shadow-xl">
-       
-    </div>
+    <div
+      id="content"
+      class="bg-terminal sm:max-w-[75vw] sm:min-w-[75vw] min-h-[60vh] rounded shadow-xl"
+    ></div>
   </div>
   <footer class="text-secondary/55 font-title">
     <span class="flex flex-col items-center justify-center">
-      <p>Made with love by <a class="underline italic" href="https://foxikle.dev">Foxikle</a> &lt;3</p>
+      <p>
+        Made with love by <a
+          class="underline italic"
+          href="https://foxikle.dev"
+        >
+          Foxikle</a
+        > &lt;3
+      </p>
       <span class="flex gap-4">
-        <a href="https://github.com/Foxikle/MultiTerm" class="underline italic">Source Code</a>
+        <a
+          href="https://github.com/Foxikle/MultiTerm"
+          target="_blank"
+          class="underline italic">Source Code</a
+        >
         <a href="/about" class="underline italic">About</a>
       </span>
     </span>
